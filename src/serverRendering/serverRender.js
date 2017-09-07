84dad12ea6f25b3b509a5310473a2de73e999e05
Xin Jin <Xin.Jin@ynap.com>
2017-09-07 11:36:23 +0100

    remove unused package

--- src/serverRendering/serverRender.js
+++ src/serverRendering/serverRender.js
@@ -2,7 +2,7 @@
  * Created by x.jin on 05/09/2017.
  */
 import { createMemoryHistory } from 'history'
-import { StaticRouter } from 'react-router'
+import { StaticRouter } from 'react-router-dom'
 import App from '../components/app'
 import ReactDOMServer from 'react-dom/server'
 import React from 'react'
